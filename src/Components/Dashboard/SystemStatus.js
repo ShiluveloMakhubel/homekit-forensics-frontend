4259e89318ff8a78d82d832d144804b277ad527a
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-08-09 14:50:04 +0200

    System status added

---
+++ src/Components/Dashboard/SystemStatus.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SystemStatus.css'; // Import the CSS file
+
+const SystemStatus = () => {
+    return (
+        <div className="card system-status">
+            <h2>System Status</h2>
+            {/* Add system status content here */}
+        </div>
+    );
+};
+
+export default SystemStatus;
