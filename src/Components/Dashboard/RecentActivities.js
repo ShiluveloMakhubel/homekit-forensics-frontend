8a286cc7bcccadb57f5330c39179654568763556
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-08-09 14:49:48 +0200

    recent activities added

---
+++ src/Components/Dashboard/RecentActivities.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './RecentActivities.css'; // Import the CSS file
+
+const RecentActivities = () => {
+    return (
+        <div className="card recent-activities">
+            <h2>Recent Activities</h2>
+            {/* Add recent activities content here */}
+        </div>
+    );
+};
+
+export default RecentActivities;
