d57bf89a5e3a8e27a570c0a2ef8b54eadc0243ff
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-08-09 14:49:28 +0200

    device overview added

---
+++ src/Components/Dashboard/DeviceOverview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DeviceOverview.css'; // Import the CSS file
+
+const DeviceOverview = () => {
+    return (
+        <div className="card device-overview">
+            <h2>Device Overview</h2>
+            {/* Add device overview content here */}
+        </div>
+    );
+};
+
+export default DeviceOverview;
