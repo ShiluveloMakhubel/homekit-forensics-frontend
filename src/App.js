9305c68ba960ffd873be2c63bb371b462fade5e9
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-08-12 21:20:23 +0200

    routes updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Dashboard from './Components/Dashboard/Dashboard';
 import DeviceDetails from './Components/DeviceDetails/DeviceDetails';
 import Settings from './Components/Settings/Settings';
 import Logs from './Components/Logs/Logs';
+import Login from './Components/Login/Login'
+
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/device-details" element={<DeviceDetails />} />
                 <Route path="/settings" element={<Settings />} />
                 <Route path="/logs" element={<Logs />} />
