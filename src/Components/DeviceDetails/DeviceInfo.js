590bd2772cf503371c1771314e996be88246b4d4
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-10-11 00:54:47 +0200

    Update DeviceInfo.js

--- src/Components/DeviceDetails/DeviceInfo.js
+++ src/Components/DeviceDetails/DeviceInfo.js
@@ -1,11 +1,53 @@
 // src/components/DeviceDetails/DeviceInfo.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const DeviceInfo = () => {
+    const [devices, setDevices] = useState([]);
+    useEffect(() => {
+        const apiUrl = 'http://localhost:5000/api/devices'; // Ensure this URL is correct and CORS is configured
+
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => setDevices(data))
+            .catch(error => console.error('Error fetching data:', error));
+    }, []);
     return (
-        <div>
-            <h2>Device Information</h2>
-            {/* Add device info details here */}
+        <div >
+            <h2>Device Overview</h2>
+            {devices.length > 0 ? (
+                <ul>
+                    {devices.map((device, index) => (
+                        <><li key={index}>
+                            <strong>{device.serviceName}</strong> -
+
+                           
+                            {device.manufaturer}
+                            {device.model}
+                            {device.port}
+                            {device.firmware}
+
+                        </li>
+                        {device.ipAddress}
+                            
+                            
+                            <div>{device.model}</div>
+                            
+                            
+                            <div>{device.port}</div>
+                            
+                            
+                            <div>{device.firmware}</div>
+                            </>
+                    ))}
+                </ul>
+            ) : (
+                <p>No devices found</p>
+            )}
         </div>
     );
 };
